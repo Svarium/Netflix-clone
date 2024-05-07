44212c24c060045aae0d8b349413faace598f5da
Svarium <svariumfoo@gmail.com>
2024-05-07 09:26:13 -0300

    arreglo bug del login y signup

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,68 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import NavBar from '../components/NavBar';
-import BackgroundImage from '../assets/home.jpg';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import backgroundImage from "../assets/home.jpg";
 import MovieLogo from "../assets/homeTitle.webp";
-import {FaPlay} from "react-icons/fa";
-import {AiOutlineInfoCircle} from "react-icons/ai";
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchMovies, getGenres } from '../store';
-import Slider from '../components/Slider';
 
-export default function Netflix() {
-    const [isScrolled, setIsScrolled] = useState(false);
-    const navigate = useNavigate();
-    const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
-    const movies = useSelector((state) => state.netflix.movies);
-   
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchMovies, getGenres } from "../store";
+import { FaPlay } from "react-icons/fa";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import Slider from "../components/Slider";
+import Navbar from "../components/NavBar";
+function Netflix() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const movies = useSelector((state) => state.netflix.movies);
+  const genres = useSelector((state) => state.netflix.genres);
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
 
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-      dispatch(getGenres())
-    },[])
+  useEffect(() => {
+    dispatch(getGenres());
+  }, []);
 
-    useEffect(() => {
-      if(genresLoaded) dispatch(fetchMovies({type:"all"}))
-    },[])
+  useEffect(() => {
+    if (genresLoaded) {
+      dispatch(fetchMovies({ genres, type: "all" }));
+    }
+  }, [genresLoaded]);
 
-    
-    window.onscroll = () => {
-        setIsScrolled(window.pageYOffset === 0 ? false : true);
-        return () => (window.onscroll = null);
-      };
- 
-     
-   return (<Container>
-    <NavBar isScrolled={isScrolled} />
-    <div className="hero">
-      <img
-        src={BackgroundImage}
-        alt="background"
-        className="background-image"
-      />
-      <div className="container">
-        <div className="logo">
-          <img src={MovieLogo} alt="Movie Logo" />
-        </div>
-        <div className="buttons flex">
-          <button
-            onClick={() => navigate('/player')}
-            className="flex j-center a-center"
-          >
-            <FaPlay />
-            Play
-          </button>
-          <button className="flex j-center a-center">
-            <AiOutlineInfoCircle />
-            More Info
-          </button>
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (!currentUser) navigate("/login");
+  });
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  return (
+    <Container>
+      <Navbar isScrolled={isScrolled} />
+      <div className="hero">
+        <img
+          src={backgroundImage}
+          alt="background"
+          className="background-image"
+        />
+        <div className="container">
+          <div className="logo">
+            <img src={MovieLogo} alt="Movie Logo" />
+          </div>
+          <div className="buttons flex">
+            <button
+              onClick={() => navigate("/player")}
+              className="flex j-center a-center"
+            >
+              <FaPlay />
+              Play
+            </button>
+            <button className="flex j-center a-center">
+              <AiOutlineInfoCircle />
+              More Info
+            </button>
+          </div>
         </div>
       </div>
-    </div> 
-     <Slider movies={movies}/>
-  </Container>)
+      <Slider movies={movies} />
+    </Container>
+  );
 }
 
 const Container = styled.div`
@@ -114,3 +123,4 @@ const Container = styled.div`
     }
   }
 `;
+export default Netflix;
